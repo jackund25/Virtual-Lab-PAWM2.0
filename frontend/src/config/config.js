d913c0a5f23085acb919ffb85b812579518e5b78
jackund25 <117989978+jackund25@users.noreply.github.com>
2024-11-18 13:12:45 +0700

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://backend-b39qnd3i3-jackund25s-projects.vercel.app'  // URL production backend
+  ? 'https://backend-nine-ebon.vercel.app/'  // URL production backend
   : 'http://localhost:8000';  // URL development
